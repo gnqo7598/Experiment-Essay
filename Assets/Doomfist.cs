fba010433e9e992f99618c31ae42ad05ae752783
gnqo7598 <gnqo7598@gmail.com>
2020-11-12 04:59:03 +0900

    DDay Stuff

---
+++ Assets/Doomfist.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Doomfist : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.AddForce(Vector3.right * 4388.4f, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
---
+++ Assets/Reinhardt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Reinhardt : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.AddForce(Vector3.left * 2644.94f, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
